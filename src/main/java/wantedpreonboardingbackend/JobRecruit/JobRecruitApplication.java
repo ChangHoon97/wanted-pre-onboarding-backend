8757b77c0718baa8e46fc47dd874bbe4bbce3930
ChangHoon <hyo04044@naver.com>
2023-10-02 22:04:31 +0900

    init commit

---
+++ src/main/java/wantedpreonboardingbackend/JobRecruit/JobRecruitApplication.java
@@ -0,0 +1,13 @@
+package wantedpreonboardingbackend.JobRecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRecruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wantedpreonboardingbackend/JobRecruit/JobRecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package wantedpreonboardingbackend.JobRecruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
